42c9d5cd76c22c8d7e4e535e9435da80186061ed
yyy <2484218565@qq.com>
2023-02-13 22:17:05 +0800

    1.1完成注册登录页面

---
+++ pages/login/login.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<!-- 编写login登录页面 -->
+		
+		<!-- 静态显示 -->
+			<!-- 设置mode=“widthFixed”保证图像不失真 -->
+		<image class="login-logo" src="../../static/logo-1.png" mode="widthFixed"></image>
+		<view class="logo-title">线上答疑系统</view>
+		<view class="release-title">v-20</view>
+		<!-- 登录按钮 -->
+		<button class="login-button" open-type="getUserInfo" @tap="doLogin()">登录</button>
+		<!-- 超链接到注册页面 -->
+		<view class="register-container">
+			没有账号？
+			<text class="register-link" @tap="goRegiater()">点击登录</text>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			goRegiater:function(){
+				uni.navigateTo({
+					url: '/pages/register/register',
+				});
+			}
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+
+</style>
---
+++ pages/register/register.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<image class="register-logo" src="../../static/logo-2.png" mode="widthFixed"></image>
+		<!-- 输入框输入邀请码 -->
+		<view class="invite-code">
+			<input class="invite-inputer" type="text" placeholder="请输入邀请码" maxlength="6" v-model="registerCode"/>
+			<view class="shuoming">管理员创建员工账号后，你可以从你的个人邮箱中获取注册邀请码</view>
+		</view>
+		<button class="register-button" open-type="getUserInfo" @tap="doRegister()">注册账号</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("register.less");
+
+</style>
